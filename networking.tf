866cea8fa999a0b289e330c804f5d16f4738f885
Hari Kammana <hari.kammana@gmail.com>
2018-08-05 12:05:13 +0530

    First week code

---
+++ networking.tf
@@ -0,0 +1,51 @@
+# Add VPC
+resource "aws_vpc" "myapp_vpc" {
+  cidr_block       = "${var.vpc_cidr}"
+  instance_tenancy = "${var.vpc_tenancy}"
+
+  tags {
+    Name  = "myapp_vpc"
+    Batch = "Weekends"
+  }
+}
+
+# Add 2 public subnets
+resource "aws_subnet" "web_subnets" {
+  count             = "${length(var.web_subnets_cird)}"
+  vpc_id            = "${aws_vpc.myapp_vpc.id}"
+  cidr_block        = "${var.web_subnets_cird[count.index]}"
+  availability_zone = "${data.aws_availability_zones.azs.names[count.index]}"
+
+  tags {
+    Name = "PublicSubnet-${count.index + 1}"
+  }
+}
+
+# Add Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.myapp_vpc.id}"
+
+  tags {
+    Name = "myapp-igw"
+  }
+}
+
+# Add Route table for public subnets
+resource "aws_route_table" "web_rt" {
+  vpc_id = "${aws_vpc.myapp_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+
+  tags {
+    Name = "myapp_web_rt"
+  }
+}
+
+resource "aws_route_table_association" "web_rt_association" {
+  count          = "${length(aws_subnet.web_subnets.*.id)}"
+  subnet_id      = "${aws_subnet.web_subnets.*.id[count.index]}"
+  route_table_id = "${aws_route_table.web_rt.id}"
+}
